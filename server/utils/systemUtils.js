be9b08ece7bafd4bc9f4499e81821af9108910e1
pas341 <philipasanne@hotmail.com>
2025-04-02 11:17:08 +0200

    Enhance getMaxRam to retrieve memory size using WMIC and provide a default value on error

--- server/utils/systemUtils.js
+++ server/utils/systemUtils.js
@@ -1,5 +1,6 @@
 const { get } = require('http');
 const os = require('os');
+const { execSync } = require('child_process');
 
 var util, config;
 
@@ -9,7 +10,17 @@ exports.systemUtils = {
         config = scripts.config;
     },
     getMaxRam: () => {
-        return Math.round(os.totalmem() / (1024)); // Ensure it returns a rounded number in GB
+        try {
+            const totalMem = execSync('wmic OS get TotalVisibleMemorySize /Value', { encoding: 'utf8' });
+            const match = totalMem.match(/TotalVisibleMemorySize=(\d+)/);
+            if (match) {
+                return Math.round(parseInt(match[1], 10) / 1024); // Convert KB to MB
+            }
+            throw new Error('Unable to parse memory size');
+        } catch (error) {
+            console.error('Error fetching total memory using command:', error);
+            return 2048; // Default to 2GB if the command fails
+        }
     },
     getOperatingSystem: () => {
         return os.type();
