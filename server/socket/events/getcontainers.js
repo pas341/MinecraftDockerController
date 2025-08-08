11dcecfe6fd4f2831b1e7aa9d4ee61e176e9a585
pas341 <philipasanne@hotmail.com>
2025-08-08 19:12:40 +0200

    Add logging for skipMemory parameter in getcontainers event

--- server/socket/events/getcontainers.js
+++ server/socket/events/getcontainers.js
@@ -16,6 +16,7 @@ exports.event = {
     register: async (socket) => {
         socket.on(`getcontainers`, async (skipMemory, callback) => {
             try {
+                console.log(`getcontainers event triggered with skipMemory: ${skipMemory}`);
                 let containers = await dockerManager.getContainers();
                 let output = [];
 
