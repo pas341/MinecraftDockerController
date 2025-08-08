e7b54e3444bf5ab37d976fece69d6a2a09730213
pas341 <philipasanne@hotmail.com>
2025-08-08 13:43:32 +0200

    Add initial implementation of ping event with socket handling

---
+++ server/socket/events/ping.js
@@ -0,0 +1,24 @@
+var socket, identity, config, scripts, util, self, dockerManager, systemUtils, socketConfig;
+
+const fs = require('fs');
+const machineIdSync = require(`node-machine-id`).machineIdSync;
+
+exports.event = {
+    init: async (s, socket, sc) => {
+        scripts = s;
+        self = this.event;
+        util = scripts.util;
+        config = scripts.config.main;
+        dockerManager = scripts.managers.docker;
+        systemUtils = scripts.utils.system;
+        identity = scripts.identity;
+        socketConfig = sc;
+        socket = socket;
+    },
+    register: async (socket) => {
+        socket.on(`ping`, async (callback) => {
+            callback({ code: 200, message: `pong`, timestamp: new Date().toISOString() });
+        });
+    },
+
+}
