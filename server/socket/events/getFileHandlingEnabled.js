1fe21d8d1af5143993b0c5d155478e7f8c071b7b
pas341 <philipasanne@hotmail.com>
2025-09-03 23:57:14 +0200

    Implement file handling event with configuration check

---
+++ server/socket/events/getFileHandlingEnabled.js
@@ -0,0 +1,25 @@
+var socket, identity, config, scripts, util, self, dockerManager, systemUtils, socketConfig;
+
+const fs = require('fs');
+const { get } = require('http');
+const machineIdSync = require(`node-machine-id`).machineIdSync;
+
+exports.event = {
+    init: async (s, socket, sc) => {
+        scripts = s;
+        self = this.event;
+        util = scripts.util;
+        config = scripts.config.main;
+        dockerManager = scripts.managers.docker;
+        systemUtils = scripts.utils.system;
+        identity = scripts.identity;
+        socketConfig = sc;
+        socket = socket;
+    },
+    register: async (socket) => {
+        socket.on(`isFileHandlingEnabled`, async (start, callback) => {
+            callback({ code: 200, value: config.fsenabled === true });
+        });
+    },
+
+}
