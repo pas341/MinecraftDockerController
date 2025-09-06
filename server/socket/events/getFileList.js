d5c0e3a4a8c0a05a1848ac54e7dbce9ca482b227
pas341 <philipasanne@hotmail.com>
2025-09-06 15:41:30 +0200

    Refactor file listing logic to use recursive function for improved directory traversal

--- server/socket/events/getFileList.js
+++ server/socket/events/getFileList.js
@@ -4,6 +4,20 @@ const fs = require('fs');
 const { get } = require('http');
 const machineIdSync = require(`node-machine-id`).machineIdSync;
 
+// Recursive function to get all files in folder and subfolders
+function getAllFiles(dirPath, arrayOfFiles = []) {
+    const files = fs.readdirSync(dirPath);
+    files.forEach(function (file) {
+        const fullPath = dirPath + '/' + file;
+        if (fs.statSync(fullPath).isDirectory()) {
+            getAllFiles(fullPath, arrayOfFiles);
+        } else {
+            arrayOfFiles.push(fullPath);
+        }
+    });
+    return arrayOfFiles;
+}
+
 exports.event = {
     init: async (s, socket, sc) => {
         scripts = s;
@@ -18,9 +32,15 @@ exports.event = {
     },
     register: async (socket) => {
         if (config.fsenabled) {
+
+
             socket.on(`listFiles`, async (folder, callback) => {
-                const fileList = fs.readdirSync(folder);
-                callback({ code: 200, content: fileList });
+                try {
+                    const fileList = getAllFiles(folder);
+                    callback({ code: 200, content: fileList });
+                } catch (err) {
+                    callback({ code: 500, message: 'Error reading files', error: err.message });
+                }
             });
         }
     },
