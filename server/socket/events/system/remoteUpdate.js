38f5c18bb3d04e0c1a30843aea30e60e87edd67d
pas341 <philipasanne@hotmail.com>
2025-12-03 04:54:53 +0100

    Refactor remote update event to improve error handling and ensure proper callback execution

--- server/socket/events/system/remoteUpdate.js
+++ server/socket/events/system/remoteUpdate.js
@@ -15,22 +15,28 @@ exports.event = {
     },
     register: async (socket) => {
         socket.on(`system/remoteupdate`, async (callback) => {
-            // Run git pull in the base directory
-            exec('git pull', { cwd: process.cwd() }, (error, stdout, stderr) => {
-                if (error) {
-                    console.error(`git pull error: ${error.message}`);
-                    res.status(500).json({ error: 'Failed to update backend', details: error.message });
-                    return;
-                }
-                console.log(`git pull stdout: ${stdout}`);
-                if (stderr) console.error(`git pull stderr: ${stderr}`);
-                res.status(200).json({ message: 'remote updated successfully', output: stdout });
-            });
+            try {
 
-            await callback({ code: 200, message: `System update initiated. Server will restart shortly.` });
-            setTimeout(() => {
-                process.exit(0);
-            }, 500);
+                // Run git pull in the base directory
+                exec('git pull', { cwd: process.cwd() }, (error, stdout, stderr) => {
+                    if (error) {
+                        console.error(`git pull error: ${error.message}`);
+                        res.status(500).json({ error: 'Failed to update backend', details: error.message });
+                        return;
+                    }
+                    console.log(`git pull stdout: ${stdout}`);
+                    if (stderr) console.error(`git pull stderr: ${stderr}`);
+                    res.status(200).json({ message: 'remote updated successfully', output: stdout });
+                });
+                
+                await callback({ code: 200, message: `System update initiated. Server will restart shortly.` });
+                setTimeout(() => {
+                    process.exit(0);
+                }, 500);
+            } catch (e) {
+                console.error(e);
+                await callback({ code: 1, message: `error during system update` });
+            }
         });
     },
 
